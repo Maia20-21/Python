edbf344c7614730a2075a15de43d92ac526dc0fb
Maia20-21 <maiabruna70@gmail.com>
2024-01-26 17:20:59 -0300

    Update b6) tipos e saídas.py

--- b6) tipos e saídas.py
+++ b6) tipos e saídas.py
@@ -10,13 +10,20 @@ n2 = bool(input('Digite outro valor: '))
 print(n2)
 
 n3 = input('Digite algo: ') 
-print(n3.isnumeric())                                          # é número?
+print('É numérico? ', n3.isnumeric())                                                            # é número?
  
 n4 = input('Digite outra coisa: ')
-print(n4.isalpha())                                            # é letra?
+print('É alfabético?', n4.isalpha())                                                             # é letra?
 
 n5 = input('Digite algo de alguma coisa: ')
-print(n5.isalnum())                                            # é letra ou número?
+print('É composto por números ou letras? ', n5.isalnum())                                        # é letra ou número?
 
 n6 = input('Digite uma palavra: ')
-print(n6.isupper())                                            # somente com letras maiúsculas?
+print('Esta tudo em letra maiúscula? ', n6.isupper())                                            # somente com letras maiúsculas?
+
+n7 = input('Digite seu nome: ')
+print('Está capitalizada? ', n7.istitle())                                                       
+
+n8 = input('aperto o espaço e de "enter":')
+print('Só tem espaços? ', n8.isspace())
+
