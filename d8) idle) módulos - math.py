5cda1dbca089a8630f49dfe5df492ac9b5f3713e
bruninhaa19 <br2020maia@gmail.com>
2024-01-30 11:29:07 -0300

    Create d8) idle) módulos - math.py

---
+++ d8) idle) módulos - math.py
@@ -0,0 +1,6 @@
+import math                                          # from math import sqrt
+num = int(input('Digite um número: '))
+raíz = math.sqrt(num)
+
+print(f'A raíz de {num} é {raíz: math.ceil}')
+
