03654f1751e6f5dcd5603839b283b36d1531855d
bruninhaa19 <br2020maia@gmail.com>
2024-01-30 14:49:53 -0300

    Create ex019.py

---
+++ ex019.py
@@ -0,0 +1,10 @@
+import random 
+
+a1 = input('Aluno 1: ')
+a2 = input('Aluno 2: ')
+a3 = input('Aluno 3: ')
+a4 = input('Aluno 4: ')
+
+x = random.random(a1, a2, a3, a4)
+
+print(f'{x} irá apagar o quadro')
