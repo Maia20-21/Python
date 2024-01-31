65c9ecac043ed101211ffbb92953765d631d3336
Maia20-21 <maiabruna70@gmail.com>
2024-01-30 23:16:54 -0300

    Create c7)operadores matematicos.py

---
+++ c7)operadores matematicos.py
@@ -0,0 +1,5 @@
+nome = input('Qual é o seu nome? ')
+
+print(f'Prazer em te conhecer, {nome}')
+print(f'Olá, {nome:^20}')                             # no centro de 20 caractéres
+print(f'Olá, {nome:=^20}'.upper())
