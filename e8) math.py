eca52155d1778bbc7136d27405a3dc016ccfad26
Maia20-21 <maiabruna70@gmail.com>
2024-02-12 13:05:52 -0300

    Create e8) math.py

---
+++ e8) math.py
@@ -0,0 +1,7 @@
+import math                                          # from math import sqrt   
+num = float(input('Digite um número: '))
+raíz = math.sqrt(num)
+
+print(f'A raíz de {num} é, aproximadamente, {math.ceil(raíz)}')
+
+
