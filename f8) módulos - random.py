edf84f7d258ba5ddb90195f8e495aec458e38cbf
Maia20-21 <maiabruna70@gmail.com>
2024-02-12 16:04:29 -0300

    Create f8) módulos - random.py

---
+++ f8) módulos - random.py
@@ -0,0 +1,11 @@
+import random 
+num = random.random()        # número aleatório entre 0-1 
+print(num)
+
+import random
+num = random.randint(1, 10)         # número aleatório entre 1 - 10
+print(num)
+
+
+import random 
+random.shuffle(x)                  # embaralhar uma lista
