c13fdf44dc8508067708e0f0a5569d28eab5d99e
Maia20-21 <maiabruna70@gmail.com>
2024-02-12 13:05:54 -0300

    Create e8) módulos - random.py

---
+++ e8) módulos - random.py
@@ -0,0 +1,8 @@
+import random 
+num = random.random()        # número aleatório entre 0-1 
+print(num)
+
+import random
+num = random.randint(1, 10)         # número aleatório entre 1 - 10
+print(num)
+
