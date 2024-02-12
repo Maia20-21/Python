48f579f8b220dfb6b32ed722b5c79ea64f85c0d0
Maia20-21 <maiabruna70@gmail.com>
2024-02-12 16:04:25 -0300

    Delete e8) módulos - random.py

--- e8) módulos - random.py
+++
@@ -1,8 +0,0 @@
-import random 
-num = random.random()        # número aleatório entre 0-1 
-print(num)
-
-import random
-num = random.randint(1, 10)         # número aleatório entre 1 - 10
-print(num)
-
