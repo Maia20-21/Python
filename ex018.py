01bfde10d50b0026dccb8cdefaf0e19424f1145b
Maia20-21 <maiabruna70@gmail.com>
2024-02-12 13:05:59 -0300

    Delete ex018.py

--- ex018.py
+++
@@ -1,7 +0,0 @@
-import math
-x = float(input('Digite o valor de um ângulo: '))
-sen = math.sin(x)
-cos = math.cos(x)
-tang = math.tan(x)
-
-print(f'Seno = {sen} \nCosseno = {cos} \nTangente = {tang}')
